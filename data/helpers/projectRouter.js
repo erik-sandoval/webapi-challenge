d020e646950f9555c5ab5195cc8a1b030c96a410
erik-sandoval <erik.sandoval@icloud.com>
2019-05-31 08:23:47 -0700

    basic server set up

---
+++ data/helpers/projectRouter.js
@@ -0,0 +1,8 @@
+const express = require("express");
+
+const Action = require("./actionModel");
+const Project = require("./projectModel");
+
+const router = express.Router();
+
+// CUSTOM MIDDLEWARE
--- index.js
+++ index.js
@@ -13,3 +13,10 @@ I need this code, just don't know where, perhaps should make some middleware, do
 Go code!
 */
 
+const server = require("./server");
+
+const port = process.env.PORT || 4000;
+
+server.listen(port, () => {
+  console.log(`listening on port ${port}`);
+});
